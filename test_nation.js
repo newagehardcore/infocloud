3981e0b12a6e2ccf85d2f674a6cadfe01d646447
ADR <adr@TAGA6917M.local>
2025-04-17 14:53:39 -0400

    chore: cleanup codebase - remove unused files, old server implementation, and outdated components

--- test_nation.js
+++
@@ -1,1 +0,0 @@
-const Parser = require('rss-parser'); const parser = new Parser(); async function test() { try { const feed = await parser.parseURL('https://www.thenation.com/feed/'); console.log(feed.title); console.log('Items:', feed.items.length); console.log('First item:', feed.items[0].title); } catch (e) { console.error('Error:', e); } } test();
